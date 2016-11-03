94c34effb4660d8e61120add2ed5c710f62ebdb1
Andreja-PC\Andreja <and>
2016-11-03 16:01:14 +0100

    -Every time new person joins the room, everybody are notified -Every time person leaves the room, everybody are notified         -Fixed bug with clearing window upon leaving         -As long as at least one person is in the room, room will exist. Room name is irrelevant, since roomID is making room unique -Code reformatted a little bit

--- routes.js
+++ routes.js
@@ -9,142 +9,139 @@ var gravatar = require('gravatar');
 // Export a function, so that we can pass 
 // the app and io instances from the app.js file:
 
-module.exports = function(app,io){
+module.exports = function (app, io) {
+    app.get('/', function (req, res) {
 
-	app.get('/', function(req, res){
+        // Render views/home.html
+        res.render('home');
+    });
 
-		// Render views/home.html
-		res.render('home');
-	});
+    app.get('/create', function (req, res) {
 
-	app.get('/create', function(req,res){
+        // Generate unique id for the room
+        var id = Math.round((Math.random() * 1000000));
 
-		// Generate unique id for the room
-		var id = Math.round((Math.random() * 1000000));
+        // Redirect to the random room
+        res.redirect('/chat/' + id);
+    });
 
-		// Redirect to the random room
-		res.redirect('/chat/'+id);
-	});
+    app.get('/chat/:id', function (req, res) {
 
-	app.get('/chat/:id', function(req,res){
+        // Render the chant.html view
+        res.render('chat');
+    });
 
-		// Render the chant.html view
-		res.render('chat');
-	});
+    // Initialize a new socket.io application, named 'chat'
+    var chat = io.on('connection', function (socket) {
 
-	// Initialize a new socket.io application, named 'chat'
-	var chat = io.on('connection', function (socket) {
+        // When the client emits the 'load' event, reply with the
+        // number of people in this chat room
+        socket.on('load', function (data) {
+            var room = findClientsSocket(io, data);
+            if (room.length === 0) {
+                socket.emit('peopleinchat', {number: 0});
+            }
+            else {
+                socket.emit('peopleinchat', {
+                    number: room.length,
+                    user: room[room.length - 1].username,
+                    avatar: room[room.length - 1].avatar,
+                    roomName: room[room.length - 1].roomName,
+                    id: data
+                });
+            }
+        });
 
-		// When the client emits the 'load' event, reply with the 
-		// number of people in this chat room
+        // When the client emits 'login', save his name and avatar,
+        // and add them to the room
+        socket.on('login', function (data) {
+            var room = findClientsSocket(io, data.id);
 
-		socket.on('load',function(data){
+            // Use the socket object to store data. Each client gets
+            // their own unique socket object
+            socket.username = data.user;
+            socket.room = data.id;
+            socket.avatar = gravatar.url(data.avatar, {s: '140', r: 'x', d: 'mm'});
+            socket.roomName = data.roomName;
 
-			var room = findClientsSocket(io,data);
-			if(room.length === 0 ) {
-				socket.emit('peopleinchat', {number: 0});
-			}
-			else {
-				socket.emit('peopleinchat', {
-					number: room.length,
-					user: room[room.length-1].username,
-					avatar: room[room.length-1].avatar,
-					roomName: room[0].roomName,
-					id:data
-				});
-			}
-		});
+            // Tell the person what he should use for an avatar
+            socket.emit('img', socket.avatar);
 
-		// When the client emits 'login', save his name and avatar,
-		// and add them to the room
-		socket.on('login', function(data) {
+            // Add the client to the room
+            socket.join(data.id);
 
-			var room = findClientsSocket(io, data.id);
+            //let everybody know that you are in the room
+            socket.broadcast.to(this.room).emit('joined', {
+                boolean: true,
+                room: this.room,
+                user: this.username,
+                avatar: this.avatar
+            });
 
-				// Use the socket object to store data. Each client gets
-				// their own unique socket object
+            var usernames = [],
+                avatars = [];
+            for (var i in room) {
+                if (Object.prototype.hasOwnProperty.call(room, i)) {
+                    usernames.push(room[i].username);
+                    avatars.push(room[i].avatar);
+                }
+            }
 
-				socket.username = data.user;
-				socket.room = data.id;
-				socket.avatar = gravatar.url(data.avatar, {s: '140', r: 'x', d: 'mm'});
-				socket.roomName = data.roomName;
+            // Send the startChat event to all the people in the
+            // room, along with a list of people that are in it.
+            chat.in(data.id).emit('startChat', {
+                boolean: true,
+                id: data.id,
+                users: usernames,
+                avatars: avatars,
+                roomName: data.roomName
+            });
+        });
 
-				// Tell the person what he should use for an avatar
-				socket.emit('img', socket.avatar);
+        // Somebody left the chat
+        socket.on('disconnect', function () {
 
+            // Notify the other person in the chat room
+            // that his partner has left
+            socket.broadcast.to(this.room).emit('leave', {
+                boolean: true,
+                room: this.room,
+                user: this.username,
+                avatar: this.avatar
+            });
 
-				// Add the client to the room
-				socket.join(data.id);
+            // leave the room
+            socket.leave(socket.room);
+        });
 
-					var usernames = [],
-						avatars = [];
+        // Handle the sending of messages
+        socket.on('msg', function (data) {
 
-					for (var i in room){
-						if(Object.prototype.hasOwnProperty.call(room, i)){
-							usernames.push(room[i].username);
-							avatars.push(room[i].avatar);
-						}
-
-					}
-
-					// Send the startChat event to all the people in the
-					// room, along with a list of people that are in it.
-
-					chat.in(data.id).emit('startChat', {
-						boolean: true,
-						id: data.id,
-						users: usernames,
-						avatars: avatars,
-                        roomName: data.roomName
-					});
-
-		});
-
-		// Somebody left the chat
-		socket.on('disconnect', function() {
-
-			// Notify the other person in the chat room
-			// that his partner has left
-
-			socket.broadcast.to(this.room).emit('leave', {
-				boolean: true,
-				room: this.room,
-				user: this.username,
-				avatar: this.avatar
-			});
-
-			// leave the room
-			socket.leave(socket.room);
-		});
-
-
-		// Handle the sending of messages
-		socket.on('msg', function(data){
-
-			// When the server receives a message, it sends it to the other person in the room.
-			socket.broadcast.to(socket.room).emit('receive', {msg: data.msg, user: data.user, img: data.img});
-		});
-	});
+            // When the server receives a message, it sends it to the other person in the room.
+            socket.broadcast.to(socket.room).emit('receive', {msg: data.msg, user: data.user, img: data.img});
+        });
+    });
 };
 
-function findClientsSocket(io,roomId, namespace) {
-	var res = [],
-		ns = io.of(namespace ||"/");    // the default namespace is "/"
+function findClientsSocket(io, roomId, namespace) {
+    var res = [],
+        // the default namespace is "/"
+        ns = io.of(namespace || "/");
 
-	if (ns) {
-		for (var id in ns.connected) {
-			if(roomId) {
-				var index = ns.connected[id].rooms.indexOf(roomId) ;
-				if(index !== -1) {
-					res.push(ns.connected[id]);
-				}
-			}
-			else {
-				res.push(ns.connected[id]);
-			}
-		}
-	}
-	return res;
+    if (ns) {
+        for (var id in ns.connected) {
+            if (roomId) {
+                var index = ns.connected[id].rooms.indexOf(roomId);
+                if (index !== -1) {
+                    res.push(ns.connected[id]);
+                }
+            }
+            else {
+                res.push(ns.connected[id]);
+            }
+        }
+    }
+    return res;
 }
 
 
