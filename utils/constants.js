375cf50aa5608d6db28f1f77c331a2ecb0f674dd
Andreja-PC\Andreja <and>
2016-11-09 09:38:28 +0100

    -Adding mongo node module

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,8 @@
-var c = {
-    test : 'peopleinchat'
+modue.exports = {
+    "facebookAuth" : {
+        "clientID" : "1187153281312072",
+        "clientSecret" : "55afb13ba52784c809946e11e8163d34",
+        "callbackUrl" : "http://localhost:8080/auth/facebook/callback"
+    }
 };
 
-module.exports.c = c;
