b521696a2bb78dff0a9cb6d8216ddb9e41772323
Andreja-PC\Andreja <and>
2016-11-18 10:24:36 +0100

    -Testing production mode without logger

--- public/js/logger.js
+++ public/js/logger.js
@@ -1,7 +1,7 @@
 var levels = ['info', 'debug', 'error', 'production'];
 
 var logger = {
-    debugLevel: 'info',
+    debugLevel: 'production',
     info: (message) => {
         printLog(message, 'info');
     },
